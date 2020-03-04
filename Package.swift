b6af779696feb640ba09c729be7e1de9c8342812
keyvchan <keyvchan@gmail.com>
2020-03-04 18:24:19 +0800

    [dependance]: Add new dependance `SwiftDate` to date manipulation.

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.1.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0"),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.3.0"),
+        .package(url: "https://github.com/malcommac/SwiftDate.git", from: "6.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,6 +25,7 @@ let package = Package(
                 .product(name: "AsyncHTTPClient", package: "async-http-client"),
                 .product(name: "Logging", package: "swift-log"),
                 .product(name: "SwiftSoup", package: "SwiftSoup"),
+                .product(name: "SwiftDate", package: "SwiftDate"),
             ]
         ),
         .testTarget(
