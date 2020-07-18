b963690f00b590c26a33aa2a871baec565f1f4e8
keyvchan <keyvchan@gmail.com>
2020-07-18 12:09:04 +0800

    [dependency]: Update console-kit.

--- Package.swift
+++ Package.swift
@@ -18,9 +18,9 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0"),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.3.0"),
         .package(url: "https://github.com/Maxim-Inv/SwiftDate.git", .branch("master")),
+        .package(url: "https://github.com/vapor/console-kit", from: "4.0.0"),
 
         // Beta version, need to switch to stable when it official release.
-        .package(url: "https://github.com/vapor/console-kit", from: "4.0.0-rc.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
